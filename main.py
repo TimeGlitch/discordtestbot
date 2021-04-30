4eb8af292936ed955fbd35b98985ca9279307e81
Frank Zhang <>
2021-04-30 04:55:13 +0000

    update command infra

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from keep_alive import keep_alive
 import openai
 import discord
+from discord.ext import commands
 import os
 import requests
 import json
 import inspirobot
 from replit import db
 
-client = discord.Client()
+client = commands.Bot(command_prefix = '$')
 openai.api_key = os.environ['OPENAI_API_KEY']
 temp = openai.Completion.create(
   engine="davinci",
   prompt="Once upon a time",
   max_tokens=5
 )
-print(temp)
 
 if True:
   @client.event
@@ -23,57 +23,23 @@ if True:
     channel = client.guilds[0].text_channels[0]
     await channel.send('gday')
 
-  @client.event
-  async def on_message(message):
-    if message.author == client.user:
-      return
+  @client.command()
+  async def poggers(ctx):
+    await ctx.send(":poggers:")
 
-    if message.content.startswith('$bot'):
-      if message.member.roles.has('tester'):
-        await message.channel.send("you gucci")
-      else:
-        await message.channel.send("you not cool enough")
-    
-    if message.content.startswith('$hello'):
-      await message.channel.send("hello")
-    
-    if message.content.startswith('$inspire'):
-      quote = inspirobot.generate()
-      await message.channel.send(quote)
+  @client.command()
+  async def inspire(ctx):
+    quote = inspirobot.generate()
+    await ctx.send(quote)
 
-    if message.content.startswith('$userinfo'):
-      await message.channel.send(message.author.name)
-
-    if message.content.startswith('$openai'):
-      await message.channel.send
-
-    """
-    if message.content.startswith('$regSag'):
-      await message.channel.send(message.content.split(' '))
-      #temp = message.content.split(' ')[1]
-      #print(temp)
-      #if temp in db:
-
-      
-      #if int(len(message.content.split(' '))) <= 2:
-
-      # await message.channel.send('add true or false as an argument dumbass')
-      # return
-      #db.update({str(message.author): str(message.content.split(' ')[1])})
-    if message.content.startswith('$checkSag'):
-      if message.author in db:
-        await message.channel.send(db[message.author])
-    """
-
-
-
-
-
-
-    
-
-
+  @client.command()
+  async def gpt(ctx, text):
+    role = discord.utils.find(lambda r: r.name == 'tester', ctx.guild.roles)
+    if role in ctx.author.roles:
+      await ctx.send("you have perms")
+    else:
+      await ctx.send("you don't have perms")
 
   keep_alive()
 
-  client.run(os.getenv('TOKEN'))
+  client.run(os.environ['DISCORD_TOKEN'])
